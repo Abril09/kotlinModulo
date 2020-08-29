07cb88533456714940e76d73165c5b93fab71648
gonzalo.mora <gonzalofmora04@gmail.com>
2020-08-28 23:01:26 -0400

    final class

--- app/src/main/java/com/curso/kotilin/Kotlin2.kt
+++ app/src/main/java/com/curso/kotilin/Kotlin2.kt
@@ -1,10 +1,12 @@
 package com.curso.kotilin
 
-fun init(){
+fun main(){
     printNombre("francisco","mora",32)
     printPrefix("francisco")
     suma(1,2)
     multiplicacion(4,9)
+
+    "francisco".saludo()
 }
 
 fun printNombre(nombre:String,apellido:String,edad:Int){
@@ -23,3 +25,26 @@ fun multiplicacion(num1: Int, num2: Int): Int{
     return num1 * num2
 }
 
+fun multiPrint(vararg message:String, prefix: String){
+    for (m  in message) println("$prefix: $m ")
+}
+
+fun String.saludo(){
+    println("saludo $this")
+}
+
+fun String.despedida(){
+    println("despedida a $this")
+}
+
+open class Dog(){
+    open fun  makeNoise(){
+        println("wow wow")
+    }
+}
+
+class Yorkshire:Dog(){
+    override fun makeNoise() {
+        println("wif wif")
+    }
+}
